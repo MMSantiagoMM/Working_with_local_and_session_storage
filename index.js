aeaffa2d3713c9cbf9e17dd5a3eff843b3e923fb
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-08-26 13:50:51 -0500

    refactorización de código

--- index.js
+++ index.js
@@ -7,51 +7,6 @@ let BtnBuscar = document.querySelector(".buscar")
 let campoBuscar = document.querySelector('.buscar-pro')
 let textoBusqueda = document.querySelector('.textoBusqueda')
 
-
-Boton.addEventListener('click',()=>{
-    guardarDatos()
-    alert("Los datos fueron cargados")
-
-})
-
-
-
-function guardarDatos(){
-    if(!Nombre.value || !Presentacion.value || !Precio.value || !Imagen.value){
-        alert("Todos los campos deben de estar llenos")
-    }
-    let producto={
-        nombre: Nombre.value,
-        presentacion: Presentacion.value,
-        precio: Precio.value,
-        imagen: Imagen.value
-    }
-    cargarDatos(producto)
-}
-
-function cargarDatos(object){
-    let keyStorage = "productos" 
-    let listProducts = []
-    let localS = JSON.parse(localStorage.getItem(keyStorage))
-    if(localS){
-        listProducts = localS
-    }
-    listProducts.push(object)
-    localStorage.setItem(keyStorage,JSON.stringify(listProducts))
-}
-
-BtnBuscar.addEventListener('click',(event)=>{
-buscarProductos()
-    
-})
-
-campoBuscar.addEventListener('keydown',(event)=>{
-    if( event.key== "Enter"){
-        buscarProductos()
-        
-    }
-})
-
 function buscarProductos(){
     let keyStorage = "productos" 
     let localS = JSON.parse(localStorage.getItem(keyStorage))
@@ -67,8 +22,45 @@ function buscarProductos(){
     v2.setAttribute('src',v1)
     v2.style = "width:200px"
     textoBusqueda.innerHTML = "Producto: " + test.nombre + "<br>" + "Presentación: " + test.presentacion + "<br>" + "Precio: " + test.precio + "<br>"
-
     textoBusqueda.appendChild(v2)
-
-    
 }
+
+function guardarDatos(){
+    if(!Nombre.value || !Presentacion.value || !Precio.value || !Imagen.value){
+        alert("Todos los campos deben de estar llenos")
+    }
+    let producto={
+        nombre: Nombre.value,
+        presentacion: Presentacion.value,
+        precio: Precio.value,
+        imagen: Imagen.value
+    }
+    cargarDatos(producto)
+}
+function cargarDatos(object){
+    let keyStorage = "productos" 
+    let listProducts = []
+    let localS = JSON.parse(localStorage.getItem(keyStorage))
+    if(localS){
+        listProducts = localS
+    }
+    listProducts.push(object)
+    localStorage.setItem(keyStorage,JSON.stringify(listProducts))
+}
+
+
+BtnBuscar.addEventListener('click',(event)=>{
+buscarProductos()
+})
+
+Boton.addEventListener('click',()=>{
+    guardarDatos()
+    alert("Los datos fueron cargados")
+})
+
+campoBuscar.addEventListener('keydown',(event)=>{
+    if( event.key== "Enter"){
+        buscarProductos()
+    }
+})
+
